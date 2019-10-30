ab171355df262ea2f7070d82fe226293a5b0fa27
Brennan Proffitt <bmproffitt@gus.pittstate.edu>
2019-10-30 18:08:27 -0500

    Added the skills to the listview with icons and labels. It looks nice on uwp. Still need to set up interactivity on the skills page.

--- AboutMe/AboutMe/SkillsPage.xaml.cs
+++ AboutMe/AboutMe/SkillsPage.xaml.cs
@@ -22,9 +22,35 @@ namespace AboutMe
             this.Skills = new ObservableCollection<skillItem>();
 
             skillItem macOSSkill = new skillItem();
+            macOSSkill.Name = "macOS and iOS";
+            macOSSkill.Icon = "icons8_apple_logo.png";
 
+            skillItem xCode = new skillItem();
+            xCode.Name = "Xcode";
+            xCode.Icon = "icons8_xcode.png";
 
+            skillItem cSharp = new skillItem();
+            cSharp.Name = "C#";
+            cSharp.Icon = "icons8_c_sharp_logo.png";
+
+            skillItem excel = new skillItem();
+            excel.Name = "Microsoft Excel";
+            excel.Icon = "icons8_microsoft_excel.png";
+
+            skillItem writing = new skillItem();
+            writing.Name = "Writing";
+            writing.Icon = "icons8_pencil.png";
+
+            Skills.Add(macOSSkill);
+            Skills.Add(xCode);
+            Skills.Add(cSharp);
+            Skills.Add(excel);
+            Skills.Add(writing);
+
+            this.skillsList.ItemsSource = this.Skills;
         }
+
+        
 
         private void AboutMeButton_Clicked(object sender, EventArgs e)
         {
